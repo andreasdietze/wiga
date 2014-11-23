07451e09ec985349d45b5705d4bae8786897a4db
Andreas <stag_o_saurus@hotmail.com>
2014-11-23 15:31:24 +0100

    Initial

---
+++ kirschli/typo3conf/localconf.php
@@ -0,0 +1,23 @@
+<?php
+$TYPO3_CONF_VARS['SYS']['sitename'] = 'New TYPO3 site';
+
+	// Default password is "joh316" :
+$TYPO3_CONF_VARS['BE']['installToolPassword'] = 'bacb98acf97e0b6112b1d1b650b84971';
+
+$TYPO3_CONF_VARS['EXT']['extList'] = 'version,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin,t3editor,reports';
+
+$typo_db_extTableDef_script = 'extTables.php';
+
+## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
+
+$TYPO3_CONF_VARS['EXT']['extList'] = 'version,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin,t3editor,reports';	// Modified or inserted by TYPO3 Core Update Manager. 
+// Updated by TYPO3 Core Update Manager 07-11-10 14:18:47
+$TYPO3_CONF_VARS['SYS']['encryptionKey'] = '15dbe793744c146f41d34f8d438b936f61c778e3ee6757806b8f3eab9be24c3f64b6d8b0c2f6177b66d0d0c068962536';	//  Modified or inserted by TYPO3 Install Tool.
+$TYPO3_CONF_VARS['SYS']['compat_version'] = '4.4';	//  Modified or inserted by TYPO3 Install Tool.
+$typo_db_username = 'rhoenblick';	//  Modified or inserted by TYPO3 Install Tool.
+$typo_db_password = '12345';	//  Modified or inserted by TYPO3 Install Tool.
+$typo_db_host = 'localhost';	//  Modified or inserted by TYPO3 Install Tool.
+$typo_db = 'rhoenblick';	//  Modified or inserted by TYPO3 Install Tool.
+$TYPO3_CONF_VARS['BE']['forceCharset'] = 'utf-8';	//  Modified or inserted by TYPO3 Install Tool.
+// Updated by TYPO3 Install Tool 07-11-10 17:19:09
+?>
